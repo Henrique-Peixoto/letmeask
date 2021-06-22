59f4f1a7dc66bf5c0130ffefbb542195c9a297de
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-06-22 18:32:35 -0300

    Linking between Home and NewRoom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useHistory  } from 'react-router';
 import { Button } from '../components/Button';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -5,6 +6,12 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 
 export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -15,7 +22,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask logo" />
-          <button className="create-room">
+          <button className="create-room" onClick={navigateToNewRoom}>
             <img src={googleIconImg} alt="Ícone do Google" />
             Crie sua sala com o Google
           </button>
