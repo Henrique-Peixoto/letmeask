be9e083efd319ffabc7287496888b81591e69ffb
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-06-22 14:15:50 -0300

    Button component style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,10 @@
-// Usando typescript para especificar o tipo dos parâmetros recebidos pelo 
-// componente
-type ButtonProps = {
-  // '?' indica um parâmetro opcional
-  children?: string;
-  text?: string;
-}
+import { ButtonHTMLAttributes } from 'react';
+import '../styles/button.scss';
 
-// 'props: ButtonProps' indicia o tipo do parâmetro 'props'
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
 export function Button(props: ButtonProps) {
   return (
-    <button>{props.children || props.text || "Default text"}</button>
+    <button className="button" {...props} />
   );
 }
